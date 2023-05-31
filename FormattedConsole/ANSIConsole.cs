c7f5adf69d828cc067406a146f1208b4d33c2587
andrewk17111 <andrewk17111@gmail.com>
2023-05-31 10:00:50 -0400

    Simplify the Process
    The ANSI codes are now string constants except for custom foreground and background colors which are functions

--- FormattedConsole/ANSIConsole.cs
+++ FormattedConsole/ANSIConsole.cs
@@ -1,162 +1,117 @@
-﻿using System.Text.RegularExpressions;
+﻿namespace FormattedConsole;
 
-namespace FormattedConsole;
-
-public static class ANSIConsole
+public static class ANSICodes
 {
-    private static readonly Dictionary<string, byte> ANSI_CODES = new Dictionary<string, byte>
-    {
-        { "reset", 0 },
-        { "bold", 1 },
-        { "intense", 1 },
-        { "faint", 2 },
-        { "dim", 2 },
-        { "dark", 2 },
-        { "italic", 3 },
-        { "underline", 4 },
-        { "slowblink", 5 },
-        { "fastblink", 6 },
-        { "reverse", 7 },
-        { "invert", 7 },
-        { "conceal", 8 },
-        { "hide", 8 },
-        { "crossed", 9 },
-        { "strike", 9 },
-        { "dunderline", 21 },
-        { "rintensity", 22 },
-        { "ritalic", 23 },
-        { "runderline", 24 },
-        { "rblink", 25 },
-        { "rreverse", 27 },
-        { "rinvert", 27 },
-        { "reveal", 28 },
-        { "rcrossed", 29 },
-        { "rstrike", 29 },
-        { "fg1", 30 },
-        { "fgblack", 30 },
-        { "fg2", 31 },
-        { "fgred", 31 },
-        { "fg3", 32 },
-        { "fggreen", 32 },
-        { "fg4", 33 },
-        { "fgyellow", 33 },
-        { "fg5", 34 },
-        { "fgblue", 34 },
-        { "fg6", 35 },
-        { "fgmagenta", 35 },
-        { "fg7", 36 },
-        { "fgcyan", 36 },
-        { "fg8", 37 },
-        { "fgwhite", 37 },
-        { "fgx", 38 },
-        { "fgrgb", 38 },
-        { "fg", 39 },
-        { "fg0", 39 },
-        { "bg1", 40 },
-        { "bgblack", 40 },
-        { "bg2", 41 },
-        { "bgred", 41 },
-        { "bg3", 42 },
-        { "bggreen", 42 },
-        { "bg4", 43 },
-        { "bgyellow", 43 },
-        { "bg5", 44 },
-        { "bgblue", 44 },
-        { "bg6", 45 },
-        { "bgmagenta", 45 },
-        { "bg7", 46 },
-        { "bgcyan", 46 },
-        { "bg8", 47 },
-        { "bgwhite", 47 },
-        { "bgx", 48 },
-        { "bgrgb", 48 },
-        { "bg", 49 },
-        { "bg0", 49 },
-        { "overline", 53 },
-        { "roverline", 55 },
-        { "fg9", 90 },
-        { "fgbrightblack", 90 },
-        { "fggrey", 90 },
-        { "fggray", 90 },
-        { "fg10", 91 },
-        { "fgbrightred", 91 },
-        { "fg11", 92 },
-        { "fgbrightgreen", 92 },
-        { "fg12", 93 },
-        { "fgbrightyellow", 93 },
-        { "fg13", 94 },
-        { "fgbrightblue", 94 },
-        { "fg14", 95 },
-        { "fgbrightmagenta", 95 },
-        { "fg15", 96 },
-        { "fgbrightcyan", 96 },
-        { "fg16", 97 },
-        { "fgbrightwhite", 97 },
-        { "bg9", 100 },
-        { "bgbrightblack", 100 },
-        { "bggrey", 100 },
-        { "bggray", 100 },
-        { "bg10", 101 },
-        { "bgbrightred", 101 },
-        { "bg11", 102 },
-        { "bgbrightgreen", 102 },
-        { "bg12", 103 },
-        { "bgbrightyellow", 103 },
-        { "bg13", 104 },
-        { "bgbrightblue", 104 },
-        { "bg14", 105 },
-        { "bgbrightmagenta", 105 },
-        { "bg15", 106 },
-        { "bgbrightcyan", 106 },
-        { "bg16", 107 },
-        { "bgbrightwhite", 107 }
-    };
+    public const string reset = "\u001b[0m";
+    public const string bold = "\u001b[1m";
+    public const string intense = bold;
+    public const string faint = "\u001b[2m";
+    public const string dim = faint;
+    public const string dark = faint;
+    public const string italic = "\u001b[3m";
+    public const string underline = "\u001b[4m";
+    public const string slowblink = "\u001b[5m";
+    public const string fastblink = "\u001b[6m";
+    public const string reverse = "\u001b[7m";
+    public const string invert = reverse;
+    public const string conceal = "\u001b[8m";
+    public const string hide = conceal;
+    public const string crossed = "\u001b[9m";
+    public const string strike = crossed;
+    public const string dunderline = "\u001b[21m";
+    public const string rintensity = "\u001b[22m";
+    public const string ritalic = "\u001b[23m";
+    public const string runderline = "\u001b[24m";
+    public const string rblink = "\u001b[25m";
+    public const string rreverse = "\u001b[27m";
+    public const string rinvert = rreverse;
+    public const string reveal = "\u001b[28m";
+    public const string rcrossed = "\u001b[29m";
+    public const string rstrike = rcrossed;
+    public const string fg1 = "\u001b[30m";
+    public const string fgblack = fg1;
+    public const string fg2 = "\u001b[31m";
+    public const string fgred = fg2;
+    public const string fg3 = "\u001b[32m";
+    public const string fggreen = fg3;
+    public const string fg4 = "\u001b[33m";
+    public const string fgyellow = fg4;
+    public const string fg5 = "\u001b[34m";
+    public const string fgblue = fg5;
+    public const string fg6 = "\u001b[35m";
+    public const string fgmagenta = fg6;
+    public const string fg7 = "\u001b[36m";
+    public const string fgcyan = fg7;
+    public const string fg8 = "\u001b[37m";
+    public const string fgwhite = fg8;
+    public const string fg = "\u001b[39m";
+    public const string fg0 = fg;
+    public const string bg1 = "\u001b[40m";
+    public const string bgblack = bg1;
+    public const string bg2 = "\u001b[41m";
+    public const string bgred = bg2;
+    public const string bg3 = "\u001b[42m";
+    public const string bggreen = bg3;
+    public const string bg4 = "\u001b[43m";
+    public const string bgyellow = bg4;
+    public const string bg5 = "\u001b[44m";
+    public const string bgblue = bg5;
+    public const string bg6 = "\u001b[45m";
+    public const string bgmagenta = bg6;
+    public const string bg7 = "\u001b[46m";
+    public const string bgcyan = bg7;
+    public const string bg8 = "\u001b[47m";
+    public const string bgwhite = bg8;
+    public const string bg = "\u001b[49m";
+    public const string bg0 = bg;
+    public const string overline = "\u001b[53m";
+    public const string roverline = "\u001b[55m";
+    public const string fg9 = "\u001b[90m";
+    public const string fgbrightblack = fg9;
+    public const string fggrey = fg9;
+    public const string fggray = fg9;
+    public const string fg10 = "\u001b[91m";
+    public const string fgbrightred = fg10;
+    public const string fg11 = "\u001b[92m";
+    public const string fgbrightgreen = fg11;
+    public const string fg12 = "\u001b[93m";
+    public const string fgbrightyellow = fg12;
+    public const string fg13 = "\u001b[94m";
+    public const string fgbrightblue = fg13;
+    public const string fg14 = "\u001b[95m";
+    public const string fgbrightmagenta = fg14;
+    public const string fg15 = "\u001b[96m";
+    public const string fgbrightcyan = fg15;
+    public const string fg16 = "\u001b[97m";
+    public const string fgbrightwhite = fg16;
+    public const string bg9 = "\u001b[100m";
+    public const string bgbrightblack = bg9;
+    public const string bggrey = bg9;
+    public const string bggray = bg9;
+    public const string bg10 = "\u001b[101m";
+    public const string bgbrightred = bg10;
+    public const string bg11 = "\u001b[102m";
+    public const string bgbrightgreen = bg11;
+    public const string bg12 = "\u001b[103m";
+    public const string bgbrightyellow = bg12;
+    public const string bg13 = "\u001b[104m";
+    public const string bgbrightblue = bg13;
+    public const string bg14 = "\u001b[105m";
+    public const string bgbrightmagenta = bg14;
+    public const string bg15 = "\u001b[106m";
+    public const string bgbrightcyan = bg15;
+    public const string bg16 = "\u001b[107m";
+    public const string bgbrightwhite = bg16;
 
-    public static string[] FORMAT_TAGS
-        => ANSI_CODES.Keys.ToArray();
+    public static string ForegroundRgb(byte r, byte g, byte b)
+        => $"\u001b[38;2;{r};{g};{b}m";
 
-    /// <summary>
-    /// Writes the specified string value, formatted using format tags to the standard output stream.
-    /// </summary>
-    /// <param name="text">The string to be written.</param>
-    public static void Write(string text)
-        => Console.Write(FormatString(text));
+    public static string ForegroundN(byte n)
+        => $"\u001b[38;5;{n}m";
 
-    /// <summary>
-    /// Writes the specified string value, formatted using format tags, followed by the current line terminator, to the
-    /// standard output stream.
-    /// </summary>
-    /// <param name="text">The string to be written.</param>
-    public static void WriteLine(string text)
-        => Console.WriteLine(FormatString(text));
+    public static string BackgroundRgb(byte r, byte g, byte b)
+        => $"\u001b[48;2;{r};{g};{b}m";
 
-    /// <summary>
-    /// Replaces format tags with their corresponding ANSI escape codes.
-    /// </summary>
-    /// <param name="text">The text to be formatted.</param>
-    /// <returns>A string with the proper ANSI escape codes.</returns>
-    public static string FormatString(string text)
-        => Regex.Replace(text, @"\{[A-Za-z0-9;,]+\}", new MatchEvaluator(ReplaceTag), RegexOptions.IgnoreCase);
-
-    /// <summary>
-    /// Replaces the format tag with the proper ANSI escape code.
-    /// </summary>
-    /// <param name="match">The matched format tag.</param>
-    /// <returns>The ANSI escape code to apply the proper formats.</returns>
-    private static string ReplaceTag(Match match)
-    {
-        string clean = Regex.Replace(match.Value, @"[\{\}\s]", "").Replace(',', ';');
-        string[] tags = clean.Split(';');
-
-        for (int i = 0; i < tags.Length; i++)
-        {
-            if (ANSI_CODES.ContainsKey(tags[i]))
-                tags[i] = ANSI_CODES[tags[i]].ToString();
-            else if (!Byte.TryParse(tags[i], out _))
-                // TODO Implement exception.
-                continue;
-        }
-
-        return $"\u001b[{String.Join(";", tags)}m";
-    }
+    public static string BackgroundN(byte n)
+        => $"\u001b[48;5;{n}m";
 }
